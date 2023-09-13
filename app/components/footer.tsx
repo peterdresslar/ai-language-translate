98a42fb3c30ad6a720cffdb211701140a272bad1
peterdresslar <peterdresslar@gmail.com>
2023-09-12 17:48:15 -1000

    Disclaimer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -5,48 +5,51 @@ import Container from "./container";
 export default function Footer() {
   return (
     <Container>
-      <div className="flex flex-wrap mx-0 md:mx-0 justify-center md:justify-between">
-          <a href="https://pacificbroadband.org" className="flex min-w-[300px] items-center space-x-2 text-xl md:text-2xl font-medium text-indigo-500 dark:text-gray-100">
-            <Image
-              src="/img/PBDE.png"
-              alt="PBDE"
-              width="32"
-              height="32"
-              className="w-8"
-            />
-            <span>Pacific Broadband and Digital Equity</span>
-          </a>
-
-        {/* <div className="mt-5">
-              <a
-                href="https://amsamoa.edu"
-                target="_blank"
-                rel="noopener"
-                className="relative block w-44">
-                <Image
-                  src="/img/ascc.png"
-                  alt="ASCC"
-                  width="32"
-                  height={0}
-                />
-              </a>
-            </div> */}
+      <div className="flex flex-wrap mx-0 md:mx-0 items-start justify-center md:justify-between">
+        <a href="https://pacificbroadband.org" className="flex min-w-[300px] items-center space-x-2 text-xl md:text-2xl font-medium text-indigo-500 dark:text-gray-100">
+          <Image
+            src="/img/PBDE.png"
+            alt="PBDE"
+            width="32"
+            height="32"
+            className="w-8"
+          />
+          <span>Pacific Broadband and Digital Equity</span>
+        </a>
 
         <div className="min-w-[300px] text-center md:text-end">
-          <div className="flex flex-wrap w-full lg:ml-0">
-            <a href="https://www.pacificbroadband.org/2023/09/01/can-ai-grasp-pacific-languages-inside-our-push-for-an-ai-samoan-translator/" className="w-full mt-2 ml-3 text-gray-500 dark:text-gray-400">
-              Blog
+          <div className="flex flex-wrap w-full justify-center md:justify-end">
+            <a href="https://www.pacificbroadband.org/2023/09/01/can-ai-grasp-pacific-languages-inside-our-push-for-an-ai-samoan-translator/" className="w-auto mt-2 ml-3 text-gray-500 dark:text-gray-400">
+              Blog Post 1
             </a>
           </div>
-          <div className="flex flex-wrap w-full lg:ml-0">
-            <a href="https://github.com/peterdresslar/ai-language-translate" className="w-full mt-2 ml-3 text-gray-500 dark:text-gray-400">
+          <div className="flex flex-wrap w-full justify-center md:justify-end">
+            <a href="https://www.pacificbroadband.org/2023/09/12/ai-translator-part-2-building-and-testing-the-app/" className="w-auto mt-2 ml-3 text-gray-500 dark:text-gray-400">
+              Blog Post 2
+            </a>
+          </div>
+          <div className="flex flex-wrap w-full justify-center md:justify-end">
+            <a href="https://github.com/peterdresslar/ai-language-translate" className="flex w-auto mt-2 ml-3 text-gray-500 dark:text-gray-400 items-center">
+              <svg height="16" width="16" viewBox="0 0 16 16" className="mr-2">
+                <path fill="currentColor" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+              </svg>
               Source Code
             </a>
           </div>
         </div>
+
+      </div> {/* end of the main footernav */}
+
+      <div className="flex my-2 font-bold justify-center text-gray-600 dark:text-gray-400">
+        <a
+          href="https://www.pacificbroadband.org/disclaimer/"
+          target="_blank"
+          rel="noopener">
+          Disclaimer
+        </a>{" "}
       </div>
 
-      <div className="my-10 text-sm text-center text-gray-600 dark:text-gray-400">
+      <div className="flex mb-10 text-sm justify-center text-gray-600 dark:text-gray-400">
         Copyright © {new Date().getFullYear()}. Peter Dresslar,&nbsp;
         <a
           href="https://pacificbroadband.org/"
@@ -58,16 +61,3 @@ export default function Footer() {
     </Container>
   );
 }
-
-const Github = ({ size = 24 }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={size}
-    height={size}
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    fillRule="evenodd"
-    clipRule="evenodd">
-    <path xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" />
-  </svg>
-);
